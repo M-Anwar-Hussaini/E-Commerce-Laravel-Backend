43a7aeccaaedc2870e15c67b2dd46220ecd3aaf5
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:28:30 +0430

    Create the relationships for Size Model

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Size extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
