6867d355a690f9fd3f0ee6644de1bbcb925d600c
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 10:31:21 +0430

    Create orders migration

---
+++ database/migrations/2024_09_01_053117_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->decimal('total', 8, 2);
+            $table->dateTime('delivered_at')->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('coupon_id')->nullable()->constrained()->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
