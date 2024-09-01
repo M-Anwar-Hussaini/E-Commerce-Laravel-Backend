91fbacf2c222b66b469daba16811d87715627ea2
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:28:01 +0430

    Create the relationships for Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'quantity',
+        'total',
+        'delivered_at',
+        'user_id',
+        'coupon_id'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function coupon()
+    {
+        return $this->belongsTo(Coupon::class);
+    }
 }
