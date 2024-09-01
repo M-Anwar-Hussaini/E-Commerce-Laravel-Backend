d866894229527e67601f4f6335e5119699be1e0e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:27:40 +0430

    Create the relationships for Color Model

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
