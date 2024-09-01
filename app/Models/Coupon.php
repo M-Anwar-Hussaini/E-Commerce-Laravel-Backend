44db56f20207916138d252cf13439b5d25ae3626
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:27:51 +0430

    Create the relationships for Coupon Model

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Coupon extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'discount',
+        'valid_until'
+    ];
+
+    /**
+     * convert coupon name to uppercase
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::upper($value);
+    }
+
+    /**
+     * checks if coupon is valid
+     */
+    public function checkIfValid()
+    {
+        return $this->valid_until > Carbon::now();
+    }
+
 }
