2fcfaab82b2bebdd8d08d31fe7cc1b739e535f2d
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:28:13 +0430

    Create the relationships for Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,44 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'quantity',
+        'price',
+        'description',
+        'thumbnail',
+        'first_image',
+        'second_image',
+        'third_image',
+        'status',
+    ];
+
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class);
+    }
+
+    public function sizes()
+    {
+        return $this->belongsToMany(Size::class);
+    }
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class)
+            ->with('user')
+            ->where('approved', true)
+            ->latest();
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
 }
