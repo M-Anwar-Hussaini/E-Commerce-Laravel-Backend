32287e5eec5b9496ff9c806faa1bcb5ff6302ef3
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:28:39 +0430

    Create the relationships for User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,14 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'address',
+        'city',
+        'zip_code',
+        'country',
+        'country',
+        'phone_number',
+        'profile_image',
+        'profile_completed',
     ];
 
     /**
@@ -43,5 +51,18 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class)->with('products')->latest();
+    }
+
+    public function image_path()
+    {
+        if ($this->profile_image) {
+            return asset('storage/images/users/' . $this->profile_image);
+        }
+        return 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
     }
 }
