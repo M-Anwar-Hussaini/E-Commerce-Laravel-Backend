504d55cba7393afe41ec89591ff5ff5e56e36020
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 10:31:01 +0430

    Create product migration

---
+++ database/migrations/2024_09_01_053049_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->longText('description');
+            $table->string('thumbnail');
+            $table->string('first_image')->nullable();
+            $table->string('second_image')->nullable();
+            $table->string('third_image')->nullable();
+            $table->string('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
