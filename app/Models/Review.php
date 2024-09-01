7350713575082596cd6ca297b397d802cd40ea0e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-01 12:28:21 +0430

    Create the relationships for Review Model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,10 +2,33 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+        'approved',
+        'rating',
+        'product_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->diffForHumans();
+    }
 }
