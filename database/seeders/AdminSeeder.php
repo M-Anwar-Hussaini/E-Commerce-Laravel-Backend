b207155da4da4f42e72b773bc7808179b9d0d7e8
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-03 10:10:30 +0430

    Add the admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
@@ -12,6 +12,6 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Admin::factory(1)->create();
     }
 }
