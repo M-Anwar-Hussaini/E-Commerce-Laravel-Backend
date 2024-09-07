c5a1142430e17f5abb2b78c2f1ed936f4abaf47d
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-07 11:46:48 +0430

    Create the header.

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,11 @@
+<header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+  <a class="navbar-brand col-md-3 col-lg-2 fs-6 me-0 px-3 text-white" href="{{ route('admin.index') }}">React T-Shirts</a>
+  <ul class="navbar-nav d-md-none flex-row">
+    <li class="nav-item text-nowrap">
+      <button class="nav-link px-3 text-white" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" type="button"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </li>
+  </ul>
+</header>
