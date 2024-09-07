183e11a58bd502a46c6096922c06be76e859d23f
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-09-07 09:34:36 +0430

    Refactor the routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
+
+Route::middleware('admin')->name('admin.')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('index');
 });
